f031781ebb25c2ba104d25ef65dca5b7fac2835e
diivanych <diivanych@list.ru>
2023-02-08 23:13:34 +0300

    001 Hello World

---
+++ Examples/Example001_HelloConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, 2 World!");
+Console.WriteLine("Hello, 2 World!");
+Console.WriteLine("Hello, 2 World!");
+Console.WriteLine("Hello, 2 World!");
