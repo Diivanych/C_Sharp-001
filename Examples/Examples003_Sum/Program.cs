8ee1bb2b036afab91759dd31fd54a93b284a4867
diivanych <diivanych@list.ru>
2023-02-09 23:11:02 +0300

    Случайные числа в сумме

--- Examples/Examples003_Sum/Program.cs
+++ Examples/Examples003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);  //Случайное число от 1 --- 9
+int numberB = new Random().Next(1,10);  //Случайное число от 1 --- 9
+Console.Write(numberA);
+Console.Write(" + ");
+Console.Write(numberB);
+Console.Write(" = ");
 int result = numberB + numberA;
 Console.WriteLine(result);
