e5d4cc356ff0aab5ce9c99bd69904202e74a4e79
diivanych <diivanych@list.ru>
2023-02-11 18:02:44 +0300

    Sum-2

---
+++ Examples/Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random().Next(1,10);  //Случайное число от 1 --- 9
+int numberB = new Random().Next(1,10);  //Случайное число от 1 --- 9
+Console.Write(numberA);
+Console.Write(" + ");
+Console.Write(numberB);
+Console.Write(" = ");
+int result = numberB + numberA;
+Console.WriteLine(result);
